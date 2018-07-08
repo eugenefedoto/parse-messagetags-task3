60c55fdeca194f28edcf67227ede07c053c1ff5a
Eugene Fedotov <eugenefedoto@gmail.om>
2018-07-08 13:37:52 -0500

    Update script for any length message-tags

--- scripts.js
+++ scripts.js
@@ -1,67 +1,44 @@
 function parse(message){
-    // message = "@aaa=bbb;ccc;example.com/ddd=eee"
-    // aaa, ccc, and ddd are tags
-    // bbb and eee are values
-
-
-    const tags = message.split(';');
-    // tags[0] is @aaa=bbb
-    // tags[1] is ccc
-    // tags[2] is example.com/ddd=eeez
+    const tags = message.substr(1).split(';'); // remove @ before getting tags
     
-    let temp = tags[0].substring(1); // aaa=bbb
+    // <tag>    ::= <key> ['=' <escaped value>]
+    // <key>    ::= [ <vendor> '/' ] <sequence of letters, digits, hyphens (`-`)>
 
-    temp = temp.split('=');
-    const firstTag = {
-        key: temp[0], // aaa
-        value: convertEscaped(temp[1]) // bbb
+    let tagsKVStore = []; 
+    for (let i = 0; i < tags.length; i++) {
+        let tempArray = tags[i].split('=');
+        let vendor = '';
+
+        // separate <vendor> from tag
+        if(tempArray[0].includes('/')){
+            const tempSplitVendor = tempArray[0].split('/')
+            vendor = tempSplitVendor[0];
+            tempArray[0] = tempSplitVendor[1];
+        }
+
+        // we don't want undefined keys
+        if(tempArray[1] === undefined){
+            tempArray[1] = '';
+        }
+
+        tagsKVStore.push({
+            vendor,
+            key: tempArray[0],
+            value: convertEscaped(tempArray[1])
+        })
     }
-
-    const secondTag = tags[1] // ccc
-
-    temp = tags[2].split('/');
-    // temp[0] is example.com
-    // temp[1] is ddd=eee
-
-    const software = temp[0];
-
-    temp = temp[1].split('=');
-    const thirdTag = {
-        key: temp[0], // ddd
-        value: convertEscaped(temp[1]) // eee
-    }
-
-    // test
-    console.log(firstTag);
-    console.log(secondTag);
-    console.log(software);
-    console.log(thirdTag);
+    console.log(tagsKVStore)
+    
+    return tagsKVStore;
 }
 
 function convertEscaped(escapedSequence){
-    const cleanedSequence = escapedSequence.replace(/\\\\/g, ";").replace(/\\s/g, " ")
-    .replace(/\\\\\\\\/g, "\\\\").replace(/\\r/g, "\r").replace(/\\n/g, "\n");
-
-    // for (let i = 0; i < escapedSequence.length; i++) {
-    //     let curChar = escapedSequence.charAt(i);
-
-    //     if(curChar === '\\:'){
-    //         cleanedSequence += ';';
-    //     } else if (curChar === '\\s') {
-    //         cleanedSequence += ' ';
-    //     } else if (curChar === '\\\\') {
-    //         cleanedSequence += '\\';
-    //     } else if (curChar === '\\r') {
-    //         cleanedSequence += 'r';
-    //     } else if (curChar === '\\n') {
-    //         cleanedSequence += '\n';
-    //     } else {
-    //         cleanedSequence += curChar;
-    //     }
-    // }
+    const cleanedSequence = escapedSequence.replace(/\:/g, ";").replace(/\\s/g, " ")
+    .replace(/\\\\/g, "\\").replace(/\\r/g, "\r").replace(/\\n/g, "\n");
 
     return cleanedSequence;
 }
 
-// parse('@aaa=bbb;ccc;example.com/ddd=eee');
-// parse('@aaa=bbb;ccc;example.com/ddd=\\se');
+// test cases
+//parse('@aaa=bbb;x=y;ccc;www;example.com/ddd=eee');
+//parse('@aaa=\rbb\nb;\\\\ccc;ffff=\:;example.com/ddd=\\se');
